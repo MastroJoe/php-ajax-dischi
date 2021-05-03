c80c7b186d9ff0af61b79ff127be067a5d6c3a62
Giorgio <giorgio.pignattari@gmail.it>
2021-05-03 18:49:51 +0200

    milestone1 e milestone2 finite

--- data.php
+++ data.php
@@ -1,40 +1,74 @@
 <?php
-    $database = [
+    $albums = [
         [
             'title' => 'New Jersey',
             'author' => 'Bon Jovi',
             'year' => 1988,
             'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+            'genre' => 'Rock',
         ],
         [
             'title' => 'Live at Wembley 86',
             'author' => 'Queen',
             'year' => 1992,
             'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
+            'genre' => 'Pop',
         ],
         [
             'title' => 'Ten\'s Summoner\'s Tales',
             'author' => 'Sting',
             'year' => 1993,
             'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
+            'genre' => 'Pop',
         ],
         [
             'title' => 'Steve Gadd band',
             'author' => 'Steve Gadd Band',
             'year' => 2018,
             'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
+            'genre' => 'Jazz',
         ],
         [
             'title' => 'Brave new World',
             'author' => 'Iron Maiden',
             'year' => 2000,
             'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
+            'genre' => 'Metal',
         ],
         [
             'title' => 'One more car, one more rider',
             'author' => 'Eric Clapton',
             'year' => 2002,
             'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+            'genre' => 'Pop',
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+            'genre' => 'Pop',
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+            'genre' => 'Pop',
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+            'genre' => 'Pop',
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+            'genre' => 'Pop',
         ]
     ];
 ?>
